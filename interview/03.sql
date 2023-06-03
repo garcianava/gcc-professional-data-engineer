3ec185ae23d5ff22b4c2ee5d652e399a20591744
José Luis García Nava <garcianava@gmail.com>
2023-06-03 16:33:33 -0600

    Update 03.sql

--- interview/03.sql
+++ interview/03.sql
@@ -1,4 +1,5 @@
-SELECT t.city_id, c.name AS city_name, t.user_sum_fare_rank_desc, t.user_id, u.name, t.user_sum_fare
+SELECT t.city_id, c.name AS city_name, t.user_sum_fare_rank_desc, t.user_id, u.name, t.user_sum_fare,
+  (SELECT SUM(fare_amount) FROM trips WHERE city_id = t.city_id) AS city_sum_fare
 FROM (
   SELECT city_id, user_id, SUM(fare_amount) AS user_sum_fare,
          ROW_NUMBER() OVER (PARTITION BY city_id ORDER BY SUM(fare_amount) DESC) AS user_sum_fare_rank_desc
@@ -8,4 +9,4 @@ FROM (
 JOIN cities c ON t.city_id = c.id
 JOIN users u ON t.user_id = u.id
 WHERE t.user_sum_fare_rank_desc <= 3
-ORDER BY t.city_id, user_sum_fare_rank_desc;
+ORDER BY city_sum_fare DESC, user_sum_fare_rank_desc;
