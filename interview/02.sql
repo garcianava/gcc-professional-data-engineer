21edc9f201e3a5e413ee1058d78deec335efb2e5
José Luis García Nava <garcianava@gmail.com>
2023-06-03 14:58:44 -0600

    Update 02.sql

--- interview/02.sql
+++ interview/02.sql
@@ -1,4 +1,6 @@
-SELECT c.id AS city_id, c.name AS city_name, SUM(t.fare_amount) AS total_fare
+SELECT c.id AS city_id, c.name AS city_name, SUM(t.fare_amount) AS city_sum_fare, 
+       (SELECT SUM(fare_amount) FROM trips) AS all_cities_sum_fare
 FROM trips t
 JOIN cities c ON t.city_id = c.id
-GROUP BY c.id, c.name;
+GROUP BY c.id, c.name
+ORDER BY city_sum_fare DESC;
