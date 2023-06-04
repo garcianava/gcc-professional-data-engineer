0d904362a67d6efaac92ed8c628431336c3412bc
José Luis García Nava <garcianava@gmail.com>
2023-06-03 18:55:39 -0600

    Update 04.py

--- interview/04.py
+++ interview/04.py
@@ -4,15 +4,14 @@ class BankAccount:
     self.history = [0]
     
   def transact(self, value):
-
+    # compute new balance
     new_balance = self.balance + value
-
+    # and verify it is positive
     if new_balance >= 0:
       self.balance = new_balance
       self.history.append(self.balance)
     else:
       print("Error: cannot apply transaction on current balance!")
-
     return self.balance
     
   def balance_history(self):
