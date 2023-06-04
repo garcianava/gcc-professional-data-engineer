1423f9487dbd4d0801b722993102d31d8b5787ce
José Luis García Nava <garcianava@gmail.com>
2023-06-04 15:12:15 -0600

    Create 07_populate_mobility.sql

---
+++ quests/build_and_optimize_data_warehouses_with_bigquery_challenge_lab/07_populate_mobility.sql
@@ -0,0 +1,25 @@
+SELECT
+  t0.country_name, t0.date,
+  t0.mobility.avg_retail, t1.avg_retail,
+  t0.mobility.avg_grocery, t1.avg_grocery,
+  t0.mobility.avg_parks, t1.avg_parks,
+  t0.mobility.avg_transit, t1.avg_transit,
+  t0.mobility.avg_workplace, t1.avg_workplace,
+  t0.mobility.avg_residential, t1.avg_residential
+FROM
+  covid19.analytics AS t0
+JOIN
+  (SELECT
+    country_region, date,
+    AVG(retail_and_recreation_percent_change_from_baseline) as avg_retail,
+    AVG(grocery_and_pharmacy_percent_change_from_baseline)  as avg_grocery,
+    AVG(parks_percent_change_from_baseline) as avg_parks,
+    AVG(transit_stations_percent_change_from_baseline) as avg_transit,
+    AVG(workplaces_percent_change_from_baseline) as avg_workplace,
+    AVG(residential_percent_change_from_baseline)  as avg_residential
+    FROM `bigquery-public-data.covid19_google_mobility.mobility_report`
+    GROUP BY country_region, date
+   ) AS t1
+ON
+   t0.country_name = t1.country_region
+AND t0.date = t1.date
