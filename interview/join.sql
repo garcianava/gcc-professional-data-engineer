e922bdd3333355b1bbe10a264a3c5c318c357319
garcianava <garcianava@gmail.com>
2023-07-11 17:54:31 -0600

    Moved interview questions to programming-exercises repository

--- interview/join.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.id AS city_id, c.name AS city_name, SUM(t.fare_amount) AS city_sum_fare, 
-       (SELECT SUM(fare_amount) FROM trips) AS all_cities_sum_fare
-FROM trips t
-JOIN cities c ON t.city_id = c.id
-GROUP BY c.id, c.name
-ORDER BY city_sum_fare DESC;
--- interview/select_where_not_in.sql
+++
@@ -1,5 +0,0 @@
-SELECT id, name
-FROM users
-WHERE id NOT IN
-(SELECT DISTINCT user_id FROM trips WHERE fare_amount >= 200)
-ORDER BY id;
